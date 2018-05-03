8226f6b38841cc2b21f1bc8b4f27fa095a797132
Dima <dimacherny3@gmail.com>
2018-05-03 16:03:44 +0300

    add page case

--- src/js/develop.js
+++ src/js/develop.js
@@ -585,3 +585,59 @@ var contacts = (function() {
     }
     // по умолчанию показываем киев
 })();
+
+/* add 03.05.2018 (project-item.html) */
+$(document).ready(function() {
+    $('.js-screenshots-slider').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        fade: true,
+        asNavFor: '.js-screenshots-slider-min',
+        responsive: [
+            {
+                breakpoint: 760,
+                settings: {
+                    asNavFor: false,
+                    autoplay: true,
+                    autoplaySpeed: 3000
+                }
+            }
+        ]
+    });
+    $('.js-screenshots-slider-min').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        asNavFor: '.js-screenshots-slider',
+        prevArrow: '<div class="s-arrow-up"><svg width="46" height="46" version="1.1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 64 64" xmlns:xlink="http://www.w3.org/1999/xlink" enable-background="new 0 0 64 64"><g><path d="M3.352,48.296l28.56-28.328l28.58,28.347c0.397,0.394,0.917,0.59,1.436,0.59c0.52,0,1.04-0.196,1.436-0.59   c0.793-0.787,0.793-2.062,0-2.849l-29.98-29.735c-0.2-0.2-0.494-0.375-0.757-0.475c-0.75-0.282-1.597-0.107-2.166,0.456   L0.479,45.447c-0.793,0.787-0.793,2.062,0,2.849C1.273,49.082,2.558,49.082,3.352,48.296z"/></g></svg></div>',
+        nextArrow: '<div class="s-arrow-down"><svg width="46" height="46" version="1.1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 129 129" xmlns:xlink="http://www.w3.org/1999/xlink" enable-background="new 0 0 129 129"><g><path d="m121.3,34.6c-1.6-1.6-4.2-1.6-5.8,0l-51,51.1-51.1-51.1c-1.6-1.6-4.2-1.6-5.8,0-1.6,1.6-1.6,4.2 0,5.8l53.9,53.9c0.8,0.8 1.8,1.2 2.9,1.2 1,0 2.1-0.4 2.9-1.2l53.9-53.9c1.7-1.6 1.7-4.2 0.1-5.8z"/></g></svg></div>',
+        focusOnSelect: true,
+        vertical: true,
+        responsive: [
+            {
+                breakpoint: 920,
+                settings: {
+                    slidesToShow: 3,
+                }
+            },
+            {
+                breakpoint: 760,
+                settings: {
+                    slidesToShow: 3,
+                }
+            }
+        ]
+    });
+    $(window).on('resize', function(){
+        var rotEl = $('.js-screenshots-slider-min');
+        var boxRotEl = $('.js-slider-min-box');
+        if ($(window).width() >= 760) {
+            rotEl.removeClass('remove-el');
+            boxRotEl.removeClass('remove-el');
+        } else {
+            rotEl.addClass('remove-el');
+            boxRotEl.addClass('remove-el');
+        }
+    });
+});
+/* END add 03.05.2018 (project-item.html) */
